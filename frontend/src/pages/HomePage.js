744e8c6897a33aea50f0f4415a7eb0776681acd3
tewarimohit <tewarimohit955@gmail.com>
2023-04-29 21:52:13 +0530

    Homepage edit

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const HomePage = () => {
-  return <h1>This is HomePage</h1>;
+  return (
+    <h1 style={{ textAlign: "center" }}>
+      Welcome to all the Amazing Events...
+    </h1>
+  );
 };
 
 export default HomePage;
